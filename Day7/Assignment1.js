313c2068157ed89717f2b46288a3d5366d7d7609
MaazMustafa <maaz.mustafa@joshsoftware.com>
2022-12-01 12:44:14 +0530

    Day7 Assignments

---
+++ Day7/Assignment1.js
@@ -0,0 +1,23 @@
+
+ //Question :-
+ //How will you create a new copy of the object below while updating the value of address.details[0] to “5“?
+
+const object1={
+ name:"Harry Potter",
+ age: 12,
+ address: {
+  details: ["4", "Privet Drive"],
+  area:"Little Whinging",
+  city: "Surrey",
+  state: "England"
+ } 
+}
+
+//object1.address.details[0]="5"
+
+//const object2={...object1,address.details:["5", "Privet Drive"]};
+const object2={...object1};
+object2.address.details[0]="5";
+
+console.log(object1);
+console.log(object2);
---
+++ Day7/Assignment2.js
@@ -0,0 +1,40 @@
+//Question :- 
+//Write a function filterObj that will filter out all the keys of a flat object that have objects
+// or arrays using Object.keys and Object.entries. Example:
+
+//---------------------- given -------------------
+let obj = {
+ a:"Apple",
+ b:["Basketball","Baseball"],
+ c: {
+  call: "cellphone"
+ },
+ d: "Dog"
+}
+console.log(filterObject(obj)); //This should return {a:”Apple”, d:”Dog”}
+
+//---------------------- my code -------------------
+
+function filterObject(obj){
+
+    //console.log(Object.entries(obj))
+    //console.log(Object.values(obj))
+    // const arr=Object.values(obj)
+    // arr.forEach((e)=>{
+    //     //console.log(typeof e);
+    //     if(typeof e !="object"){
+    //         console.log("true");
+    //     }
+    // })
+    const arrayOfEntries=Object.entries(obj);
+    let newObject={};
+    arrayOfEntries.forEach((arr)=>{
+        if(typeof arr[1] !="object")
+        {
+            let str=arr[0];
+            //newObject={arr:arr[1]}
+            newObject[`${str}`]=arr[1];
+        }
+    })
+    return newObject;
+}
