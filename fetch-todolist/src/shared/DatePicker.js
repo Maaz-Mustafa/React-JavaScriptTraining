ce544e1343cfcd92015640380b09a9c54b454bae
MaazMustafa <maaz.mustafa@joshsoftware.com>
2023-01-18 16:00:09 +0530

    removed comments

--- fetch-todolist/src/shared/DatePicker.js
+++ fetch-todolist/src/shared/DatePicker.js
@@ -7,21 +7,6 @@ const DatePicker = (props) => {
   return (
     <div className="form-control">
       <label htmlFor={name}>{label}</label>
-      {/* <Field name={name}>
-        {({ form, field }) => {
-          console.log(form);
-          console.log(field);
-          const { setFieldValue } = form;
-          const { value } = field;
-          return (
-            <Dateview
-              id={name}
-              selected={value}
-              onChange={(val) => setFieldValue(name, val)}
-            />
-          );
-        }}
-      </Field> */}
       <Field type="date" name={name} {...rest} />
       <ErrorMessage name={name} component={TextError} />
     </div>
